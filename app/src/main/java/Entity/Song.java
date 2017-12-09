c6e887f96a4b2c54f768cc8dc9120850c965d10a
Phatest1 <tanphat1309@gmail.com>
2017-12-09 14:14:41 +0700

    - ui (Phat)
    Signed-off-by: Phatest1 <tanphat1309@gmail.com>

---
+++ app/src/main/java/Entity/Song.java
@@ -0,0 +1,30 @@
+package Entity;
+
+/**
+ * Created by Le Phat on 09-Dec-17.
+ */
+
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id = songID;
+        title = songTitle;
+        artist = songArtist;
+    }
+
+    public long getID() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+}
