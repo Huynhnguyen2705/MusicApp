18e32f583c7a7fb004d515f620a194656c46a056
Huynhnguyen2705 <huynhnguyen2705@gmail.com>
2017-12-13 12:51:07 +0700

    - UI and Activity Class for playback and full screen. Not playing music yet.
    Signed-off-by: Huynhnguyen2705 <huynhnguyen2705@gmail.com>

---
+++ app/src/main/java/com/learnandroid/huynh/music_app/ActionBarCastActitvity.java
@@ -0,0 +1,13 @@
+package com.learnandroid.huynh.music_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class ActionBarCastActitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_bar_cast_actitvity);
+    }
+}
